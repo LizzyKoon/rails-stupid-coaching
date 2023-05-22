98681e531bdb6a4e9dc923e428729f5febc0c460
Maxence Audibert <audibert.maxence@gmail.com>
2023-05-22 14:25:51 +0200

    m

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,3 +1,19 @@
 class QuestionsController < ApplicationController
+  def ask
+    # @question = params[:question]
+    # @answer = answer
+    # render "views/questions/ask.html.erb"
+  end
 
+  def answer
+    @question = params[:question]
+    if @question.end_with?("?")
+      @coach_answer = "Silly question, get dressed and go to work!."
+    elsif @question == "I'm going to work"
+      @coach_answer = "Great!"
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
+    end
+    # render "views/questions/answer.html.erb"
+  end
 end
